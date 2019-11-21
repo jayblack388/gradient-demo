e9010923927ec5ced0e0d653c028c6501c5ab13e
John Blackwell <john.blackwell@rtslabs.com>
2019-11-21 12:09:26 -0800

    allow copy on bg string

--- src/components/colorForm.js
+++ src/components/colorForm.js
@@ -1,9 +1,28 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect, useRef, useState } from "react"
 import { ThemeContext } from "styled-components"
-import { Button, Field, RowDiv } from "jdb-components"
+import { Button, Field, RowDiv, BlockSpan, ColDiv } from "jdb-components"
 import { generateGradientCSSString } from "gradient-steps-string-generator"
 
+const CopyButton = ({ onClick, success }) => {
+  return (
+    <Button
+      onClick={onClick}
+      color={success ? "success" : "successSecondary"}
+      style={{
+        position: "absolute",
+        top: 0,
+        right: 0,
+        padding: `0.6rem ${success ? 1 : 1.4}rem`,
+      }}
+    >
+      {success ? "Copied!" : "Copy"}
+    </Button>
+  )
+}
+
 const GradientDemo = ({ colors, stops }) => {
+  const stringRef = useRef()
+  const [copySuccess, setCopySuccess] = useState(false)
   const {
     colors: themeColors,
     mode: { darkMode },
@@ -13,22 +32,79 @@ const GradientDemo = ({ colors, stops }) => {
     const colorValues = Object.values(colors).map(val => val) || []
     const gradientString = generateGradientCSSString(colorValues, stops)
     setBackground(gradientString)
+    setCopySuccess(false)
   }, [colors, stops])
+
+  useEffect(() => {
+    if (copySuccess) {
+      setTimeout(() => {
+        setCopySuccess(false)
+      }, 3000)
+    }
+  }, [copySuccess])
+
+  const copyCodeToClipboard = () => {
+    if (!stringRef.current) return
+    navigator.permissions
+      .query({ name: "clipboard-write" })
+      .then(function(result) {
+        if (result.state == "granted" || result.state == "prompt") {
+          navigator.clipboard.writeText(background).then(
+            function() {
+              /* clipboard successfully set */
+              setCopySuccess(true)
+            },
+            function() {
+              /* clipboard write failed */
+            }
+          )
+        }
+        // Don't do anything if the permission was denied.
+      })
+  }
   return (
-    <RowDiv
-      align="center"
-      justify="center"
-      width="80%"
-      height="50%"
-      style={{
-        background: darkMode
-          ? themeColors.light.regular
-          : themeColors.dark.regular,
-        marginTop: "1rem",
-      }}
-    >
-      <RowDiv width="95%" height="90%" style={{ background }} />
-    </RowDiv>
+    <>
+      <RowDiv
+        align="center"
+        justify="center"
+        width="80%"
+        height="40%"
+        style={{
+          background: darkMode
+            ? themeColors.light.regular
+            : themeColors.dark.regular,
+          marginTop: "1rem",
+        }}
+      >
+        <RowDiv width="95%" height="90%" style={{ background }} />
+      </RowDiv>
+      <BlockSpan style={{ marginTop: "1rem", padding: "0 20%" }}>
+        Generated value:
+      </BlockSpan>
+      <ColDiv
+        style={{
+          background: darkMode
+            ? themeColors.light.regular
+            : themeColors.dark.regular,
+        }}
+        width="60%"
+        align="center"
+      >
+        <BlockSpan
+          ref={stringRef}
+          style={{
+            color: darkMode
+              ? themeColors.dark.regular
+              : themeColors.light.regular,
+            padding: "1rem",
+            width: "60%",
+          }}
+        >
+          {background}
+        </BlockSpan>
+        <CopyButton success={copySuccess} onClick={copyCodeToClipboard} />
+      </ColDiv>
+    </>
   )
 }
 
@@ -40,7 +116,10 @@ const ColorForm = () => {
   const [stops, setStops] = useState(10)
   const addField = () => {
     setColorFields(prevValues => {
-      const newColor = `#${Math.random().toString(16).slice(2, 8).toUpperCase()}`;
+      const newColor = `#${Math.random()
+        .toString(16)
+        .slice(2, 8)
+        .toUpperCase()}`
       const currentLength = Object.keys(prevValues).length
       if (currentLength > 5) {
         return prevValues
@@ -80,7 +159,7 @@ const ColorForm = () => {
           onChange={e => setStops(e.target.value)}
         />
         <Button
-          color="successPrimary"
+          color="successSecondary"
           disabled={Object.keys(colorFields).length > 5}
           message={
             Object.keys(colorFields).length === 0
